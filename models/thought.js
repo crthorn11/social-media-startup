7506d6a9ec9f94a7335783656d7e5f37c542ff88
Cody T <crthorn11@gmail.com>
2024-04-28 20:39:19 -0400

    I managed to find a simple... bug in my serverjs file. Some code i forgot to add. dur. Fixed my thoughts and tested in insomnia

--- models/thought.js
+++ models/thought.js
@@ -1,32 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-// Thought Schema
-const thoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    minlength: 1,
-    maxlength: 280
-  },
-  username: {
-    type: String,
-    required: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (createdAtVal) => dateFormat(createdAtVal)
-  },
-  reactions: [reactionSchema]
-});
-
-thoughtSchema.virtual('reactionCount').get(function() {
-  return this.reactions.length;
-});
-
-module.exports = mongoose.model('Thought', thoughtSchema);
-
 // Reaction Schema
 const reactionSchema = new Schema({
   reactionId: {
@@ -48,3 +22,29 @@ const reactionSchema = new Schema({
     get: (createdAtVal) => dateFormat(createdAtVal)
   }
 });
+
+// Thought Schema
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => dateFormat(createdAtVal)
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  reactions: [reactionSchema]
+});
+
+thoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+module.exports = mongoose.model('Thought', thoughtSchema);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const db = require('./config/connection');
-const userRoutes = require("./routes/userRoutes")
+const userRoutes = require("./routes/userRoutes");
+const thoughtRoutes = require("./routes/thoughtRoutes");
 
 
 const PORT = process.env.PORT || 3001;
@@ -9,7 +10,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(userRoutes)
+app.use(userRoutes);
+app.use(thoughtRoutes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
